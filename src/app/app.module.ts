bae8f4697d700e5a31bc1bad9eab75e978c46eb8
Serove Awedikian <ser.avedikian@gmail.com>
2024-01-02 20:59:18 +0000

    m10 - end

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { SiteHeaderComponent } from './site-header/site-header.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-import { HttpClientModule } from '@angular/common/http';
 import { CartComponent } from './cart/cart.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SignInComponent } from './user/sign-in/sign-in.component';
+import { TemplateFormControlsComponent } from './user/template-form-controls/template-form-controls.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +20,15 @@ import { AppRoutingModule } from './app-routing.module';
     CatalogComponent,
     SiteHeaderComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    SignInComponent,
+    TemplateFormControlsComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/site-header/site-header.component.ts
+++ src/app/site-header/site-header.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IUser } from '../user/user.model';
+import { UserService } from '../user/user.service';
 
 @Component({
   selector: 'bot-site-header',
   templateUrl: './site-header.component.html',
   styleUrls: ['./site-header.component.css']
 })
-export class SiteHeaderComponent {
+export class SiteHeaderComponent implements OnInit {
+  user: IUser | null = null;
+  showSignOutMenu: boolean = false;
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
+  ngOnInit(): void {
+    this.userService.getUser().subscribe({
+      next: (user) => this.user = user
+    });
+  }
+
+  toggleSignOutMenu() {
+    this.showSignOutMenu = !this.showSignOutMenu;
+  }
+
+  signOut() {
+    this.userService.signOut();
+    this.showSignOutMenu = false;
+  }
 }
